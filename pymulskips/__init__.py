8c82aac5a4bc7220877d930d14a830249a9f3f67
Gaetano Calogero <tano@DESKTOP-2S27N6L.localdomain>
2022-02-01 18:25:04 +0100

    Enabled import of pymulskips without dependencies. Now one can (must) import individual modules inside pymulskips package.

--- pymulskips/__init__.py
+++ pymulskips/__init__.py
@@ -5,7 +5,17 @@ pymulskips
 __author__ = "Gaetano Calogero, Giuseppe Fisicaro, Ioannis Deretzis, Antonino La Magna"
 # __license__ = "MPL-2.0"
 
-from . import io
-from . import process
-from . import setuprun
-from . import analyze
+#from . import io
+#from . import process
+#from . import setuprun
+#from . import analyze
+
+import glob, os
+print("""
+  *** Welcome to PyMulSKIPS ***
+  Modules:""")
+path = os.path.dirname(os.path.realpath(__file__))
+for f in glob.glob(path+'/*.py'):
+    if f != path+'/__init__.py':
+        print('    '+os.path.splitext(os.path.basename(f))[0])
+print('')
