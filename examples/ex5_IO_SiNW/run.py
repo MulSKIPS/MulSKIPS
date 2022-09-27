1385e8ae65a08f15466431913f58749201bed4fe
MulSKIPS <gaetano.calogero@imm.cnr.it>
2022-09-27 13:12:44 +0200

    Updated ex5 SiNW example with rotated input MSH file.

--- examples/ex5_IO_SiNW/run.py
+++ examples/ex5_IO_SiNW/run.py
@@ -15,14 +15,14 @@ print(f'cashocs v{cashocs.__version__}')
 Load mesh and import it in Dolfin
 """
 
-structurename = 'si-cylinder-ox-removed-withphysicalentitiers_fps' # neglect .msh extension
+structurename = 'si-cylinder-ox-removed-rotated-withphysicalentities_fps' # neglect .msh extension
 
 # Use the line below to convert a geo into a msh file
 # call([f"gmsh -3 structurename.geo -o structurename.msh -format msh"], shell=True)
 
 rescale = 1e3 # msh by Chiara is in micron. Here we need nm. 
 mesh, subdomains, boundaries, dx, ds, dS = io.msh2dolfin(structurename+'.msh', \
-    save_h5=True, rescale=rescale, rotate_angle=90, rotate_axis=1)
+    save_h5=True, rescale=rescale)
 
 
 
@@ -85,7 +85,7 @@ NB: since this takes time, recycle existing DAT file in the current directory
 
 if not os.path.isfile(os.getenv("PWD")+f'/{structurename}_InputKMC.dat'):
     cell_map, rank_map, wall_map, lenx, leny, lenz = io.dolfin2mulskips(f'{structurename}_InputKMC.dat', \
-        mpclass, regions, mesh, subdomains, \
+        mpclass, regions, mesh, subdomains, z_reverse=False, \
         regionsfilename=f'{structurename}_KMCregions', return_box_size=True)
 else:
     print("Found DAT input file for MulSKIPS in current folder...")
@@ -152,7 +152,7 @@ PPS: dolfin2mulskips() MUST be run before this to get cell_map and wall_map.
 #     sys.exit()
 # else:
 #     subdomains = io.mulskips2dolfin(f'{structurename}_AfterKMC.dat', mpclass, mesh, subdomains, regions, \
-#         cell_map, rank_map, wall_map, pvdfilename=f'{structurename}_Mesh_AfterKMC', LA=False)
+#         cell_map, rank_map, wall_map, pvdfilename=f'{structurename}_Mesh_AfterKMC', LA=False, z_reverse=False)
 #     # Write dolfin mesh to msh format. 
 #     io.dolfin2msh(structurename+'.msh', structurename+'_final.msh', \
 #         mesh, subdomains, boundaries)
